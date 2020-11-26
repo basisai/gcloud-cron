37d68989bd2e1da28c568476deba140f3d0f1f0f
Yong Wen Chua <lawliet89@users.noreply.github.com>
2020-11-26 11:01:00 +0800

    Remove TTY

--- tests/lint.sh
+++ tests/lint.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 TEST_IMAGE="${TEST_IMAGE:-quay.io/helmpack/chart-testing:v3.0.0}"
 
-docker run --rm -it -v "$(pwd):/gcloud-cron" --workdir "/gcloud-cron" \
+docker run --rm -v "$(pwd):/gcloud-cron" --workdir "/gcloud-cron" \
     "${TEST_IMAGE}" \
 ct lint --all --config tests/ct.yaml
